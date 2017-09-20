cd835f377a48473d3200756dd55e33228ff7a5a2
littlekbt <kr.kubota.11@gmail.com>
2017-09-20 12:56:13 +0900

    slack-rtm-bot-helperをversion up

--- bot/main.rb
+++ bot/main.rb
@@ -5,14 +5,15 @@ require 'time'
 require 'pry'
 
 class SlackLambda
-
+  NAME = 'slack-lambda'
   def initialize(token)
     @token = token
+    @name  = NAME
   end
 
   def run
     puts "#{[Time.now().to_s]} open web socket connection"
-    Slack::Rtm::Bot::Helper.run(token=@token) do |data|
+    Slack::Rtm::Bot::Helper.run(token: @token, name: @name) do |data|
       text = data['text']
       case extract_action(text)
       when :register
@@ -35,53 +36,6 @@ class SlackLambda
   end
 
   private
-
-  def show_help
-  <<EOF
-slack-lambda is bot that execute function.
-
-# exec
-execute function.
-
-@slack-lambda exec
-language: [ruby|golang]
-version: <ex: 2.3.0>
-```
-# input your program
-```
-
-# register
-register function
-
-@slack-lambda register
-name: <input function name>
-language: [ruby|golang]
-version: <ex: 2.3.0>
-```
-# input your program
-```
-
-# show
-show registered function
-
-@slack-lambda show <function name>
-
-# list
-list functions
-
-@slack-lambda list 
-
-# remove
-delete function
-
-@slack-lambda remove <function name>
-
-# run
-execute registered function
-
-@slack-lambda run <function name>
-EOF
-  end
   
   CONVERT_TO_ACTION = {
     "register" => :register,
@@ -94,17 +48,17 @@ EOF
   }
 
   def extract_action(text)
-    m = text.match(/<@U7337SHAR(?:\|slack-lambda)?>\s*(register|show|list|remove|run|exec|help)/)
+    m = text.match(/>\s*(register|show|list|remove|run|exec|help)/)
 
     CONVERT_TO_ACTION[m[1]] if m && CONVERT_TO_ACTION[m[1]]
   end
 
   class UserFile
     REGISTER_REGEXP = /name:\s?(.+)\n(language:\s?.*\nversion:\s?.*\n```\n?[\s\S]*\n?```)/
-    SHOW_REGEXP     = /<@U7337SHAR(?:\|slack-lambda)?>\s*show\s*([a-z|A-Z|0-9|_|-]*)/
-    LIST_REGEXP     = /<@U7337SHAR(?:\|slack-lambda)?>\s*list/
-    REMOVE_REGEXP     = /<@U7337SHAR(?:\|slack-lambda)?>\s*remove\s*([a-z|A-Z|0-9|_|-]*)/
-    RUN_REGEXP      = /<@U7337SHAR(?:\|slack-lambda)?>\s*run\s*([a-z|A-Z|0-9|_|-]*)/
+    SHOW_REGEXP     = />\s*show\s*([a-z|A-Z|0-9|_|-]*)/
+    LIST_REGEXP     = />\s*list/
+    REMOVE_REGEXP     = />\s*remove\s*([a-z|A-Z|0-9|_|-]*)/
+    RUN_REGEXP      = />\s*run\s*([a-z|A-Z|0-9|_|-]*)/
     class << self
       def register(text)
         m = text.match(REGISTER_REGEXP)
@@ -203,6 +157,53 @@ Error Found!
 ERR
       end
     end
+  end
+
+  def show_help
+  <<EOF
+slack-lambda is bot that execute function.
+
+# exec
+execute function.
+
+@slack-lambda exec
+language: [ruby|golang]
+version: <ex: 2.3.0>
+```
+# input your program
+```
+
+# register
+register function
+
+@slack-lambda register
+name: <input function name>
+language: [ruby|golang]
+version: <ex: 2.3.0>
+```
+# input your program
+```
+
+# show
+show registered function
+
+@slack-lambda show <function name>
+
+# list
+list functions
+
+@slack-lambda list 
+
+# remove
+delete function
+
+@slack-lambda remove <function name>
+
+# run
+execute registered function
+
+@slack-lambda run <function name>
+EOF
   end
 end
 
